adefcc24190b7f9e4c95e4dcae41fb6f6d96dcb0
maximegenand <m.genand@gmx.fr>
2023-07-29 08:06:14 +0200

    correctif

--- screens/TripScreen.jsx
+++ screens/TripScreen.jsx
@@ -3,10 +3,13 @@ import { View, Text, TouchableOpacity, Button } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 // Import styles
-import { globalsStyles, GLOBAL_COLOR } from '../styles/globals'
+import { globalsStyles, GLOBAL_COLOR } from '../styles/globals';
 import styles from "../styles/TripStyles";
 
 //Import components
+// import Header  from '../components/Header';
+import BoutonAdd from '../components/BoutonAdd';
+import Event from '../components/Event'
 
 //Import modules
 
@@ -35,35 +38,23 @@ export default function TripScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.header_Left}>
-          <TouchableOpacity onPress={() => navigation.navigate('Home')} activeOpacity={0.8}>
-            <FontAwesome style={styles.fleche} name="arrow-left" size={30} color={GLOBAL_COLOR.TERTIARY} />
-          </TouchableOpacity>
-          <View>
-            <Text style={styles.name}>Rome les mecs</Text>
-            <Text style={styles.buddys}>Adrien, crackito, prout, max ...</Text>
-          </View>
-        </View>
-        <FontAwesome style={styles.bell} name="bell" size={30} color={GLOBAL_COLOR.TERTIARY} />
-      </View>
+      <Header />
       <View style={styles.planning}>
         <View style={styles.calendrier}>
           <View style={styles.fleche_left}>
-            <FontAwesome name="arrow-left" size={30} color={GLOBAL_COLOR.TERTIARY} />
+            <FontAwesome name="arrow-left" size={30} color={GLOBAL_COLOR.TERTIARY}/>
           </View>
           <View style={styles.day}>
             <Text style={styles.jour}>Aujourd'hui</Text>
             <Text style={styles.date}>23 juillet 1999</Text>
           </View>
           <View style={styles.fleche_left}>
-            <FontAwesome name="arrow-right" size={30} color={GLOBAL_COLOR.TERTIARY} />
+            <FontAwesome name="arrow-right" size={30} color={GLOBAL_COLOR.TERTIARY}/>
           </View>
         </View>
-        <View style={styles.event}>
-          <Button title="Go to event" onPress={() => navigation.navigate('Event')} />
-        </View>
+        <Event />
       </View>
+      <BoutonAdd/>
     </View>
   );
 }
