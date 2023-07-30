b99b853820f5ad429426b29fa67054d4670304bf
maximegenand <m.genand@gmx.fr>
2023-07-31 00:27:48 +0200

    create HomeScreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,13 +1,17 @@
 import { useRef, useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Button, Br } from 'react-native';
+import { View, Text, TouchableOpacity, Button, Br, StatusBar } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 // Import styles
 import { globalsStyles, GLOBAL_COLOR } from '../styles/globals'
 import styles from "../styles/HomeStyles";
 
 //Import components
+import SvgLogo from '../components/svg/SvgLogo';
+import SvgUser from '../components/svg/SvgUser';
 
 //Import modules
+import BoutonAdd from '../components/BoutonAdd';
 
 // Import redux
 import { useDispatch, useSelector } from "react-redux";
@@ -29,14 +33,43 @@ export default function HomeScreen({ navigation }) {
 
   // 3. Functions
 
+  // Fonction d'affichage de la liste des Trips
+  const tripList = trips.map(trip => (
+    <TouchableOpacity style={styles.tripContainer} key={trip.tokenTrip} onPress={() => navigation.navigate('TabNavigator')}>
+      <Text style={styles.tripTitle}>{trip.name}</Text>
+      <View style={styles.tripSubContainer}>
+        <Text style={styles.tripParticipants}>{trip.participants.length} participants</Text>
+        <Text style={styles.tripDate}>du ?? au ?? juillet</Text>
+      </View>
+    </TouchableOpacity>
+  ));
 
   // 4. Return Component
   return (
-    <View style={globalsStyles.container}>
-      <Button title="Back to Signin" onPress={() => navigation.navigate('Signin')} />
-      <Text style={globalsStyles.title}>HomeScreen</Text>
-      <Text style={globalsStyles.title}>All your trips here !</Text>
-      <Button title="Trip à Rome" onPress={() => navigation.navigate('TabNavigator')} />
+    <View style={styles.container}>
+      <StatusBar backgroundColor={GLOBAL_COLOR.TERTIARY} barStyle="dark-content" />
+      <View style={styles.header}>
+        <View style={styles.logo}>
+          <SvgLogo width={50} height={50} fill={GLOBAL_COLOR.TERTIARY} />
+          <Text style={styles.logoText}>BuddyTrip</Text>
+        </View>
+        <View style={styles.userContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate('Signin')} activeOpacity={0.8}>
+            <SvgUser width={45} height={45} fill={GLOBAL_COLOR.PRIMARY} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.listTrips}>
+          { // Si un trip est enregistré, on affiche la liste, sinon on envoie un message de remplacement
+          tripList.length ?
+            tripList
+            :
+            <Text>Aucun Trip renseigné</Text>
+          }
+        </View>
+        <View style={styles.add}><BoutonAdd /></View>
+      </View>
     </View>
   );
 }
--- styles/HomeStyles.js
+++ styles/HomeStyles.js
@@ -3,4 +3,68 @@ import { GLOBAL_COLOR } from './globals';
 const { width:screenWidth, height: screenHeight } = Dimensions.get('window');
 
 export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: GLOBAL_COLOR.TERTIARY,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 67,
+        paddingHorizontal: 10,
+        backgroundColor: GLOBAL_COLOR.PRIMARY,
+    },
+    logo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    logoText: {
+        marginLeft: 5,
+        color: GLOBAL_COLOR.TERTIARY,
+        fontSize: 26,
+        fontFamily: 'Montserrat-Alternates-SemiBold-Italic',       
+    },
+    userContainer: {
+        width: 45,
+        height: 45,
+        backgroundColor: GLOBAL_COLOR.TERTIARY,
+        borderRadius: 25,
+    },
+    body: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        paddingTop: 20,
+    },
+    listTrips: {
+        alignItems: 'center',
+        width: '100%',
+    },
+    tripContainer: {
+        width: '90%',
+
+        marginVertical: 20,
+        padding: 10,
+        backgroundColor: GLOBAL_COLOR.PRIMARY,
+        borderRadius: 10,
+    },
+    tripTitle: {
+        textAlign: 'center',
+        color: GLOBAL_COLOR.TERTIARY,
+        fontSize: 22,
+    },
+    tripSubContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    tripParticipants: {
+        color: GLOBAL_COLOR.TERTIARY
+    },
+    tripDate: {
+        color: GLOBAL_COLOR.TERTIARY
+    },
+    add: {
+        margin: 20,
+    },
 });
