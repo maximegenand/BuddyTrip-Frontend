1ce09175aa413126e9060ea467e1711ea441c282
maximegenand <m.genand@gmx.fr>
2023-08-01 22:49:11 +0200

    add formatDate & change screenPeriod name to formatPeriod

--- modules/dates.js
+++ modules/dates.js
@@ -1,24 +1,49 @@
+import { format, isSameYear, isSameMonth, getDate } from "date-fns";
+import { fr } from 'date-fns/locale'
+
 // Fonction pour formater une date au format 'jour mois' sans afficher le mois si le mois est identique entre les deux dates
-function formatDate(date, showMonth = true) {
-    const options = showMonth ? { day: '2-digit', month: 'short' } : { day: '2-digit' };
-    return date.toLocaleDateString('fr-FR', options);
+function formatDate(
+  date,               // Date à afficher en format String (peut contenir un time aussi)
+  showYear = false,   // false: affiche l'année seulement si celle-ci est différente de l'actuelle ; true: l'affiche tout le temps
+  short = false,      // false: affiche le mois en mode "décembre" ; true: affiche le mois en mode "déc."
+  range = false,      // utilisé seulement par les fonctions
+  showMonth = true    // utilisé seulement par les fonctions
+) {
+  // On raccourci le nom du mois si le paramètre true est ON
+  const month = short ? 'MMM' : 'MMMM'; 
+  // On renvoie 1er pour le jour si c'est le cas
+  let day = 'd';
+  if (getDate(date) === 1) day = 'do';
+  
+  // On vérifie le year actuel pour l'afficher si ce n'est pas le même que l'année en cours, sauf si on est dans un range
+  if (!showYear && !range) if (!isSameYear(date, new Date())) showYear = true;
+  // On retourne le format 'dd MMMM? y'
+  if (showYear) return format(date, `${day} ${month} y`, { locale: fr });
+
+  // On retourne le format 'dd MMMM?'
+  if (showMonth) return format(date, `${day} ${month}`, { locale: fr });
+
+  // Si on est dans un range et qu'on n'affiche pas le month, on retourne au format 'dd'
+  return format(date, day, { locale: fr });
   }
+
+
+  // Fonction pour afficher un range de deux dates
+  const formatPeriod = dates => {
+    // On créé la variable dateStart et dateEnd
+    const [ dateStart, dateEnd ] = dates;
   
-  // Fonction pour afficher le message
-const screenPeriod = dates => {
-    // Analyser les dates du tableau en objets de type Date
-    const dateStart = new Date(dates[0]);
-    const dateEnd = new Date(dates[1]);
+    // On vérifie que les années sont identiques
+    const sameYear = isSameYear(dateStart, dateEnd);
+    // On vérifie que les mois sont identiques
+    const sameMonth = isSameMonth(dateStart, dateEnd);
   
-    // Vérifier si les mois sont identiques
-    const sameMonth = dateStart.getMonth() === dateEnd.getMonth();
-  
-    // Formater les dates
-    const dateStartString = formatDate(dateStart, !sameMonth);
-    const dateEndString = formatDate(dateEnd);
+    // On formate les dates
+    const dateStartString = formatDate(dateStart, !sameYear, true, true, !sameMonth);
+    const dateEndString = formatDate(dateEnd, false, true);
 
     // Afficher le message
     return `du ${dateStartString} au ${dateEndString}`;
   }
 
-module.exports = { screenPeriod };
+module.exports = { formatDate, formatPeriod };
