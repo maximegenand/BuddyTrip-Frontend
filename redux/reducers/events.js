b8d091256bea55c34887a99de5ff8de9f042365f
maximegenand <m.genand@gmx.fr>
2023-08-01 22:46:57 +0200

    change default values

--- redux/reducers/events.js
+++ redux/reducers/events.js
@@ -3,50 +3,71 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const initialState = {
   value: [
     {
-        tokenEvent: "ertyuhgfdsdfghjfd",
-        tokenTrip: "sqdlkjqldkjlkjl",
-        user: {
-            tokenUser: "gdd5g1q3f5g13g13s51s3dsf12",
-            username: "Ben",
-        },
-        name: "Apéro d'arrivée",
-        date: "2023-07-24T00:00:00.000Z",
-        timeStart: "2023-07-24T18:00:00.000Z",
-        timeEnd: "2023-07-24T00:00:00.000Z",
-        place: "Centre ville",
-        description: "Petite découverte de Rome après que tout le monde se soit installé.\r\n\r\nOn va tester quelques bars du centre-ville.",
-        participants: [
-            { tokenUser: "qsdmqlksdmsldfiojensdflksd", username: "John" },
-            { tokenUser: "sfmlkfmokvmsoenzelknlk,mds", username: "Barbie" },
-            { tokenUser: "sdf6gf1s6f1e65s1f6s51f65sd", username: "Ken" },
-        ],
-        infos: [
-            { tokenInfo: "grtyrtyer", user: { tokenUser: "sfmlkfmokvmsoenzelknlk,mds", username: "Barbie" }, name: "Plan de la ville", type:"type/pdf", uri:"https://sebsauvage.net/pdf/pdfgratuit.pdf" }
-        ]
+      "tokenEvent": "UqsJhATSBSZqpgBXCDlDFyyYZdmsTS4A",
+      "tokenTrip": "FFOzaNjn2L4z_tx23_tbixr2shBM29p0",
+      "category": "activity",
+      "name": "Apéro d'arrivée",
+      "date": "2023-07-24T00:00:00.000Z",
+      "timeStart": "2023-07-24T18:00:00.000Z",
+      "place": "Centre ville",
+      "description": "Petite découverte de Rome après que tout le monde se soit installé.\r\n\r\nOn va tester quelques bars du centre-ville.",
+      "user": {
+        "tokenUser": "1rbtH6kQHjePtTK2qZu0XAkWDK7Ubmt4",
+        "username": "Ken"
+      },
+      "participants": [
+        {
+          "tokenUser": "1rbtH6kQHjePtTK2qZu0XAkWDK7Ubmt4",
+          "username": "Ken"
+        }
+      ],
+      "infos": [
+        {
+          "tokenInfo": "grRCnfG91EZCK3XwgmSitMDz5cMozgCM",
+          "user": {
+            "tokenUser": "1rbtH6kQHjePtTK2qZu0XAkWDK7Ubmt4",
+            "username": "Ken"
+          },
+          "name": "Plan de la ville",
+          "type": "type/pdf",
+          "uri": "https://sebsauvage.net/pdf/pdfgratuit.pdf"
+        }
+      ]
     },
     {
-        tokenEvent: "mxcvmxcvklxmcvlkm",
-        tokenTrip: "eilsddlknckjbzedlikj",
-        user: {
-            tokenUser: "gdd5g1q3f5g13g13s51s3dsf12",
-            username: "Ben",
-        },
-        name: "Twingo Travel",
-        date: "2023-09-15T00:00:00.000Z",
-        timeStart: "2023-09-15T18:00:00.000Z",
-        timeEnd: "2023-07-15T18:00:00.000Z",
-        place: "Lyon",
-        seats: 5,
-        description: "On part de Lyon avec ma Twingo !\r\n\r\nJ’ai 8 places, et s’il faut j’ai une remorque.",
-        participants: [
-            { tokenUser: "qsdmqlksdmsldfiojensdflksd", username: "John" },
-            { tokenUser: "sfmlkfmokvmsoenzelknlk,mds", username: "Barbie" },
-            { tokenUser: "sdf6gf1s6f1e65s1f6s51f65sd", username: "Ken" },
-        ],
-        infos: [
-            { tokenInfo: "oazipazoeipoi", user: { tokenUser: "sdf6gf1s6f1e65s1f6s51f65sd", username: "Ken" }, name: "Vla la caisse", type:"type/jpeg", uri:"https://www.rs-automobiles.com/7045-large_default/renault-twingo-1993.jpg" }
-        ]
-    },
+      "tokenEvent": "2XKxuo2My8T_ZAC84RuCO4gXx4ohWxL5",
+      "tokenTrip": "IX755iXN85pkpAqzHcqnw_Y2N4-FNEE8",
+      "category": "travel",
+      "name": "Twingo Travel",
+      "date": "2023-09-15T00:00:00.000Z",
+      "timeStart": "2023-09-15T14:00:00.000Z",
+      "timeEnd": "2023-09-15T18:00:00.000Z",
+      "place": "Lyon",
+      "description": "On part de Lyon avec ma Twingo !\r\n\r\nJ’ai 8 places, et s’il faut j’ai une remorque.",
+      "seats": 5,
+      "user": {
+        "tokenUser": "N9k2ePqtGpDnS2lwiVvpaOUBBVRGT_3M",
+        "username": "John"
+      },
+      "participants": [
+        {
+          "tokenUser": "N9k2ePqtGpDnS2lwiVvpaOUBBVRGT_3M",
+          "username": "John"
+        }
+      ],
+      "infos": [
+        {
+          "tokenInfo": "1-fqnzyhBKsE9RkpLAr5GYexugPVZJEN",
+          "user": {
+            "tokenUser": "nyVbO_NO2cXcXjYAiGMqLuKFcDpwmZBQ",
+            "username": "Barbie"
+          },
+          "name": "Vla la caisse",
+          "type": "type/jpeg",
+          "uri": "https://www.rs-automobiles.com/7045-large_default/renault-twingo-1993.jpg"
+        }
+      ]
+    }
   ],
 };
 
@@ -55,13 +76,16 @@ export const eventsSlice = createSlice({
   initialState,
   reducers: {
     addEvent: (state, action) => {
-        state.value.push = action.payload;
-      },
+      state.value.push = action.payload;
+    },
+    addAllEvents: (state, action) => {
+      state.value = action.payload
+    },
     removeAllEvents: (state, action) => {
-        state.value = [];
-      },
+      state.value = [];
+    },
   },
 });
 
-export const { addEvent, removeAllEvents } = eventsSlice.actions;
+export const { addEvent, addAllEvents, removeAllEvents } = eventsSlice.actions;
 export default eventsSlice.reducer;
--- redux/reducers/trips.js
+++ redux/reducers/trips.js
@@ -3,50 +3,72 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const initialState = {
   value: [
     {
-        tokenTrip: "sqdlkjqldkjlkjl",
-        user: {
-            tokenUser: "gdd5g1q3f5g13g13s51s3dsf12",
-            username: "Ben",
-            picture: "https://img.freepik.com/photos-gratuite/portrait-homme-blanc-isole_53876-40306.jpg?w=900&t=st=1690554398~exp=1690554998~hmac=be6a913bf58c9dd234f651a692863dd0f6226ec13a4905152d3430d925c4fd9c",
+      "tokenTrip": "FFOzaNjn2L4z_tx23_tbixr2shBM29p0",
+      "name": "Rome les mecs & filles",
+      "dateStart": "2023-07-24T00:00:00.000Z",
+      "dateEnd": "2023-08-02T00:00:00.000Z",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "user": {
+        "tokenUser": "3dqVkFvE-QwQDTT4Zg0JlmLcNuO2D3xM",
+        "username": "Ben"
+      },
+      "participants": [
+        {
+          "tokenUser": "N9k2ePqtGpDnS2lwiVvpaOUBBVRGT_3M",
+          "username": "John"
         },
-        name: "Rome les mecs",
-        dateStart: "2023-07-24T00:00:00.000Z",
-        dateEnd: "2023-08-02T00:00:00.000Z",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        participants: [
-            { tokenUser: "qsdmqlksdmsldfiojensdflksd", username: "John" },
-            { tokenUser: "sfmlkfmokvmsoenzelknlk,mds", username: "Barbie" },
-            { tokenUser: "sdf6gf1s6f1e65s1f6s51f65sd", username: "Ken" },
-        ],
+        {
+          "tokenUser": "nyVbO_NO2cXcXjYAiGMqLuKFcDpwmZBQ",
+          "username": "Barbie"
+        },
+        {
+          "tokenUser": "1rbtH6kQHjePtTK2qZu0XAkWDK7Ubmt4",
+          "username": "Ken"
+        }
+      ]
     },
     {
-        tokenTrip: "eilsddlknckjbzedlikj",
-        user: {
-            tokenUser: "sfmlkfmokvmsoenzelknlk,mds",
-            username: "Barbie",
+      "tokenTrip": "IX755iXN85pkpAqzHcqnw_Y2N4-FNEE8",
+      "name": "Titre long de 50 caractères pour gérer l'affichage",
+      "dateStart": "2023-09-15T00:00:00.000Z",
+      "dateEnd": "2023-09-30T00:00:00.000Z",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "user": {
+        "tokenUser": "N9k2ePqtGpDnS2lwiVvpaOUBBVRGT_3M",
+        "username": "John"
+      },
+      "participants": [
+        {
+          "tokenUser": "3dqVkFvE-QwQDTT4Zg0JlmLcNuO2D3xM",
+          "username": "Ben"
         },
-        name: "Titre long de 50 caractères pour gérer l'affichage",
-        dateStart: "2023-09-15T00:00:00.000Z",
-        dateEnd: "2023-09-30T00:00:00.000Z",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        participants: [
-            { tokenUser: "qsdmqlksdmsldfiojensdflksd", username: "John" },
-            { tokenUser: "gdd5g1q3f5g13g13s51s3dsf12", username: "Ben", picture: "https://img.freepik.com/photos-gratuite/portrait-homme-blanc-isole_53876-40306.jpg?w=900&t=st=1690554398~exp=1690554998~hmac=be6a913bf58c9dd234f651a692863dd0f6226ec13a4905152d3430d925c4fd9c" },
-            { tokenUser: "sdf6gf1s6f1e65s1f6s51f65sd", username: "Ken" },
-        ],
+        {
+          "tokenUser": "nyVbO_NO2cXcXjYAiGMqLuKFcDpwmZBQ",
+          "username": "Barbie"
+        },
+        {
+          "tokenUser": "1rbtH6kQHjePtTK2qZu0XAkWDK7Ubmt4",
+          "username": "Ken"
+        }
+      ]
     }
-  ],
+  ]
 };
 
 export const tripsSlice = createSlice({
   name: 'trips',
   initialState,
   reducers: {
+    updateTrip: (state, action) => {
+      const newState = state.value.filter(element => element.tokenTrip !== action.payload.tokenTrip);
+      newState.push(action.payload);
+      return state.value = newState;
+    },
     addAllTrips: (state, action) => {
       state.value = action.payload;
     },
   },
 });
 
-export const { addAllTrips } = tripsSlice.actions;
+export const { updateTrip, addAllTrips } = tripsSlice.actions;
 export default tripsSlice.reducer;
