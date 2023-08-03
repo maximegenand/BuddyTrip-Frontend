e1e27d1676cd373f66ba2f24979369ed96eb171e
maximegenand <m.genand@gmx.fr>
2023-08-03 13:40:08 +0200

    fix bar error

--- components/BuddyBubble.jsx
+++ components/BuddyBubble.jsx
@@ -1,22 +1,34 @@
 import { View, StyleSheet, Text, TouchableOpacity, Dimensions, Image } from "react-native";
 import { GLOBAL_COLOR } from '../styles/globals';
 
-export default function BuddyBubble({buddy, add, i}) {
-console.log(buddy?.image);
-
-    const textScreen = add ? `+${add}` : buddy?.username.slice(0,1).toUpperCase();
+export default function BuddyBubble(props) {
 
   return (
-    <View style={[ styles.bubble, { left: -15 * i, zIndex: 100 - i } ]}>
+    <View style={[
+      styles.bubble,
+      {
+        zIndex: 100 - props.i,
+        left: -0.4 * props.size * props.i,
+        width: props.size,
+        height: props.size,
+        borderRadius: props.size / 2,
+      }
+    ]}>
       {  // On affiche l'image de l'utilisateur si elle existe, sinon la première lettre de son nom
-      buddy?.image ? (
-        <Image
-            source={{uri: buddy.image}}
+      props.add ? (
+        <Text style={[styles.add, {fontSize: props.size / 3, paddingRight: props.size / 7}]}>+{props.add}</Text>
+      ) : (
+        props.buddy.image ? (
+          <Image
+            width={props.size}
+            height={props.size}
+            source={{uri: props.buddy.image}}
             resizeMode="cover"
-            style={styles.image}
-        />
-      ):(
-        <Text style={styles.initiale}>{textScreen}</Text>
+            style={[styles.image, { borderRadius: props.size / 2 }]}
+          />
+        ):(
+          <Text style={styles.initiale}>{props.buddy.username.slice(0,1).toUpperCase()}</Text>
+        )
       )}
     </View>
   );
@@ -27,23 +39,22 @@ const styles = StyleSheet.create({
         position: 'relative',
         justifyContent: 'center',
         alignItems: 'center',
-        width: 40,
-        height: 40,
         backgroundColor: GLOBAL_COLOR.TERTIARY,
         borderWidth: 2,
         borderColor: GLOBAL_COLOR.PRIMARY,
-        borderRadius: 20,
     },
     image: {
-        width: 40,
-        height: 40,
         borderWidth: 2,
         borderColor: GLOBAL_COLOR.PRIMARY,
-        borderRadius: 20,
     },
     initiale: {
-        color: GLOBAL_COLOR.SECONDARY,
-        fontSize: 20,
-        fontWeight: 700,
-    }
+      color: GLOBAL_COLOR.SECONDARY,
+      fontSize: 20,
+      fontWeight: 700,
+    },
+    add: {
+      alignSelf: 'flex-end',
+      color: GLOBAL_COLOR.SECONDARY,
+      fontWeight: 700,
+    },
 });
