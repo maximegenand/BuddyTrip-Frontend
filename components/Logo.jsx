5e248ea2be869ae87ba1b67471dbd7075b480f9f
maximegenand <m.genand@gmx.fr>
2023-08-03 21:04:35 +0200

    create component Logo

---
+++ components/Logo.jsx
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import { GLOBAL_COLOR } from '../styles/globals';
+import SvgLogo from "./svg/SvgLogo";
+
+export default function Logo(props) {
+  // On initialise la size à 50 si la props n'est pas envoyée
+  const size = props.size ? props.size : 50;
+  return (
+    <View style={[
+      props.style,
+      {
+        alignItems: 'center',
+      }]}>
+      <SvgLogo style={{alignSelf: 'center'}} width={size} height={size} fill={GLOBAL_COLOR.TERTIARY} />
+      <Text style={{
+        marginLeft: size / 10,
+        color: GLOBAL_COLOR.TERTIARY,
+        fontSize: size / 2,
+        fontFamily: 'Montserrat-Alternates-SemiBold-Italic',   
+      }}>BuddyTrip</Text>
+    </View>
+  );
+}
