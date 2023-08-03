6515b051ebfa9771e9ba81c4a60d4794a7d634be
rayenne <rayene_chouguiat@hotmail.com>
2023-08-03 10:01:58 +0200

    gestion de lajout dun event

--- screens/NewEventScreen.jsx
+++ screens/NewEventScreen.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect} from "react";
 import {
   View,
   Text,
@@ -41,34 +41,49 @@ export default function NewEventScreen({ navigation }) {
 
   // 2. UseEffect, UseState, UseRef
   const [titre, setTitre] = useState("");
+  // ETAT rendu conditionnel en fonction de l'event
   const [transport, setTransport] = useState(false);
   const [act, setAct] = useState(false);
+
   const [selected, setSelected] = useState("");
+  const titreRef = useRef(null);
+  const dateRef = useRef(null);
+  const heureDepartRef = useRef(null);
+  const placesDispoRef = useRef(null);
+  const numeroBilletRef = useRef(null);
+  const titreActRef = useRef(null);
+  const descriptionRef = useRef(null);
 
   // 3. Functions
 
+// choix disponible dans l'input clickable
   const data = [
     { key: "1", value: "Voiture" },
     { key: "2", value: "Train" },
     { key: "3", value: "Avion" },
   ];
 
+  // Définir les éléments JSX en fonction de la sélection
   let typeTransportInput;
+  //l'input pour ajouter le nombre de place disponible si type de transport est la voiture 
   const typeTransportVoiture = (
     <View style={styles.containerInputDesc}>
       <TextInput
+       ref={placesDispoRef}
+       onChangeText={value => placesDispoRef.current.inputValue = value}
         placeholder="Nombre de places disponible"
         style={styles.inputDesc}
         keyboardType="numeric"
       ></TextInput>
     </View>
   );
+  //l'input pour ajouter le numero de billet si le moyen de transport est train ou avion 
   const typeTransportCommun = (
     <View style={styles.containerInputDesc}>
-      <TextInput placeholder="Numero de billet" style={styles.inputDesc}></TextInput>
+      <TextInput ref={numeroBilletRef}  onChangeText={value => numeroBilletRef.current.inputValue = value}  placeholder="Numero de billet" style={styles.inputDesc}></TextInput>
     </View>
   );
-
+// gestion du transport en fonction de la selection du type de transport
   if (selected === "1") {
     typeTransportInput = typeTransportVoiture;
   } else if (selected === "") {
@@ -77,6 +92,7 @@ export default function NewEventScreen({ navigation }) {
     typeTransportInput = typeTransportCommun;
   }
 
+  // input si bouton Transport = true
   const transportForm = (
     <View style={styles.transport}>
       <SelectList
@@ -89,35 +105,37 @@ export default function NewEventScreen({ navigation }) {
         dropdownStyles={{ backgroundColor: GLOBAL_COLOR.TERTIARY }}
       />
       <View style={styles.containerInputDesc}>
-        <TextInput placeholder="Heure de Depart" style={styles.inputDesc}></TextInput>
+        <TextInput ref={heureDepartRef} onChangeText={value => heureDepartRef.current.inputValue = value} placeholder="Heure de Depart" style={styles.inputDesc}></TextInput>
       </View>
       {typeTransportInput}
-      <View style={styles.descritpion}>
-        <TextInput placeholder="Description" style={styles.inputDescription} />
-      </View>
+     /* <View style={styles.descritpion}>
+        <TextInput ref={descriptionRef}  onChangeText={value => descriptionRef.current.inputValue = value} placeholder="Description" style={styles.inputDescription} />
+      </View>*/
     </View>
   );
-
+   //  input si bouton Activity = true
+   
   const activityForm = (
     <View style={styles.transport}>
       <View style={styles.containerInputDesc}>
-        <TextInput placeholder="Heure de Depart" style={styles.inputDesc}></TextInput>
+        <TextInput ref={heureDepartRef} onChangeText={value => heureDepartRef.current.inputValue = value}  placeholder="Heure de Depart" style={styles.inputDesc}></TextInput>
       </View>
       <View style={styles.containerInputDesc}>
-        <TextInput placeholder="Titre" style={styles.inputDesc}></TextInput>
+        <TextInput ref={titreActRef}  onChangeText={value => titreActRef.current.inputValue = value}  placeholder="Titre" style={styles.inputDesc}></TextInput>
       </View>
-      <View style={styles.descritpion}>
-        <TextInput placeholder="Description" style={styles.inputDescription} />
-      </View>
+     /* <View style={styles.descritpion}>
+        <TextInput ref={descriptionRef}  onChangeText={value => descriptionRef.current.inputValue = value} placeholder="Description" style={styles.inputDescription} />
+      </View>*/
     </View>
   );
 
   let description = (
     <View style={styles.descritpion}>
-      <TextInput placeholder="Description" style={styles.inputDescription} />
+      <TextInput ref={descriptionRef} onChangeText={value => descriptionRef.current.inputValue = value} placeholder="Description" style={styles.inputDescription} />
     </View>
+   
   );
-
+ // description = input de base, si act = true => activityFormn, si transport = true => transportForm
   if (act) {
     description = activityForm;
   } else if (transport) {
@@ -136,6 +154,19 @@ export default function NewEventScreen({ navigation }) {
     setAct(true);
     setTransport(false);
   };
+  const handlePress=()=> {
+    const newTrip = {
+          titre: titreRef.current.inputValue,
+          date: dateRef.current.inputValue, 
+          heureDepart: heureDepartRef.current.inputValue,
+          placeDispo: placesDispoRef.current.inputValue,
+          numBillet: numeroBilletRef.current.inputValue,
+          titreAct: titreActRef.current.inputValue,
+          description: descriptionRef.current.inputValue,
+    
+  }
+  console.log(newTrip)
+  }
 
   // return (
   //   <>
@@ -171,8 +202,8 @@ export default function NewEventScreen({ navigation }) {
           <Text style={styles.name}>Nouvelle Activité</Text>
         </View>
         <View style={styles.forms}>
-          <Form placeholder="Titre" onChangeText={setTitre} value={titre} />
-          <Form placeholder="JJ/MM/AAAA" />
+          <Form   ref= {titreRef}  placeholder="Titre" onChangeText={setTitre} value={titre} />
+          <Form ref={dateRef} onChangeText={value => dateRef.current.inputValue = value} placeholder="JJ/MM/AAAA" />
           <View style={styles.categorie}>
             <Text style={styles.textCategorie}>Selectionner une categorie :</Text>
             <View style={styles.bulles}>
@@ -193,7 +224,7 @@ export default function NewEventScreen({ navigation }) {
           <View style={styles.variable}>
             {description}
             <TouchableOpacity style={styles.enregistrer}>
-              <Text style={styles.enregistrerWord}>Enregistrer</Text>
+              <Text style={styles.enregistrerWord} onPress={() => handlePress()}>Enregistrer</Text>
             </TouchableOpacity>
           </View>
         </View>
