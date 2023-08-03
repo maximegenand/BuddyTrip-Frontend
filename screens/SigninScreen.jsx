a54b5d27cc6c7871a60bf7f819f20f80555cb60d
maximegenand <m.genand@gmx.fr>
2023-08-03 21:13:48 +0200

    add login screen

--- screens/SigninScreen.jsx
+++ screens/SigninScreen.jsx
@@ -1,17 +1,19 @@
 import { useRef, useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, TextInput, Button } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity, TextInput, Button, StatusBar } from 'react-native';
+import { BACK_URL } from '@env';
 
 // Import styles
 import { globalsStyles, GLOBAL_COLOR } from '../styles/globals'
 import styles from "../styles/SigninStyles";
 
 //Import components
+import Logo from '../components/Logo';
 
 //Import modules
 
 // Import redux
 import { useDispatch, useSelector } from "react-redux";
-import { updateName, resetUser } from '../redux/reducers/user';
+import { login } from '../redux/reducers/user';
 import {  } from '../redux/reducers/trips';
 import {  } from '../redux/reducers/events';
 
@@ -25,27 +27,112 @@ export default function SigninScreen({ navigation }) {
   const dispatch = useDispatch();
 
   // 2. UseEffect, UseState, UseRef
-  const inputRef = useRef(null);
+  const [ errorFetch, setErrorFetch ] = useState(null);
+  const [ disabled, setDisabled ] = useState(false);
+  const [ email, setEmail] = useState("");
+  const [ password, setPassword] = useState("");
+
+  // Permet de supprimer le message d'erreur dès que l'utilisateur tape un nouveau texte
+  useEffect(() => {
+    if (errorFetch) setErrorFetch(null);
+}, [email, password])
 
 
   // 3. Functions
 
+  // Fonction de connection
+  const handleConnect = async () => {
+    // On annule l'action si le formulaire est disabled
+    if (disabled) return;
+
+    // On change la valeur du disabled le temps du fetch
+    setDisabled(true);
+
+    // On vérifie si les inputs sont renseignés et ne sont pas vides
+    if(!(email && email !== "" && password && password !== "")) {
+      setDisabled(false);
+      setErrorFetch('Empty fields');
+      return;
+    }
+    try {
+      // On envoie la donnée au backend
+      const fetchLogin = await fetch(`${BACK_URL}/users/signin`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({email, password}),
+      });
+      const data = await fetchLogin.json();
+
+      // Si on a result False, on affiche un message à l'utilisateur
+      if (!data.result) {
+        setDisabled(false);
+        setErrorFetch(data.error);
+        return;
+      }
+
+      // On reset les inputs et on redirige sur le home du user
+      setPassword("");
+      dispatch(login(data.user));
+      navigation.navigate("Home");
+      setDisabled(false);
+    } catch (error) {
+      setDisabled(false);
+      setErrorFetch("Erreur de connexion au serveur");
+      console.error("Erreur lors de l'envoi au serveur :", error);
+    }
+  }
+
 
   // 4. Return Component
   return (
-    <View style={globalsStyles.container}>
-      <Text style={globalsStyles.title}>SigninScreen</Text>
-      <Button title="Go to Signup" onPress={() => navigation.navigate('Signup')} />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Text style={{marginTop: 50}}>Test pour redux :</Text>
-      <Text>username : {user.username}</Text>
-      <TextInput
-        placeholder='New username'
-        ref={inputRef}
-        onChangeText={value => inputRef.inputValue = value}
-      />
-      <Button title='Change username' onPress={() => dispatch(updateName(inputRef.inputValue))} />
-      <Button title="Reset Initial User value" onPress={() => dispatch(resetUser())} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor={GLOBAL_COLOR.SECONDARY} barStyle="light-content" />
+      <Logo size={100} style={styles.logo} />
+      <View style={styles.body}>
+        <Text style={styles.error}>{errorFetch}</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <Text onPress={() => {setEmail('john@gmail.com'); setPassword('azerty');}}>John</Text>
+          <Text onPress={() => {setEmail('barbie@gmail.com'); setPassword('azerty');}}>Barbie</Text>
+          <Text onPress={() => {setEmail('ken@gmail.com'); setPassword('azerty');}}>Ken</Text>
+          <Text onPress={() => {setEmail('ben@gmail.com'); setPassword('azerty');}}>Ben</Text>
+        </View>
+        <View style={[styles.inputContainer, disabled && styles.inputContainerDisabled]}>
+          <TextInput
+            editable={!disabled}
+            style={[styles.input, disabled && styles.inputDisabled]}
+            placeholder="Email"
+            autoComplete="email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            onChangeText={setEmail}
+            value={email}
+          />
+        </View>
+        <View style={[styles.inputContainer, disabled && styles.inputContainerDisabled]}>
+          <TextInput
+            editable={!disabled}
+            style={[styles.input, disabled && styles.inputDisabled]}
+            placeholder="Password"
+            autoComplete="current-password"
+            autoCapitalize="none"
+            secureTextEntry={true}
+            onChangeText={setPassword}
+            value={password}
+          />
+        </View>
+        <TouchableOpacity style={[styles.linkContainer, {alignSelf: 'flex-end'}]} onPress={() => console.log('mdp oublié')}>
+          <Text style={[styles.linkText, {textAlign: 'right'}]} activeOpacity={0.8}>Mot de passe oublié</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.btnConnect, disabled && styles.btnConnectDisabled]} onPress={handleConnect} activeOpacity={0.8}>
+          <Text style={[styles.textConnect, disabled && styles.textConnectDisabled]}>Se connecter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.linkContainer,{alignSelf: 'center'}]} onPress={() => navigation.navigate('Signup')}>
+          <Text style={[styles.linkText, {textAlign: 'center'}]} activeOpacity={0.8}>Créer un compte</Text>
+        </TouchableOpacity>
+      </View>
+      <View></View>
+    </SafeAreaView>
   );
 }
--- styles/SigninStyles.js
+++ styles/SigninStyles.js
@@ -3,4 +3,74 @@ import { GLOBAL_COLOR } from './globals';
 const { width:screenWidth, height: screenHeight } = Dimensions.get('window');
 
 export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: GLOBAL_COLOR.SECONDARY,
+  },
+  logo: {
+    alignContent: 'center',
+    textAlign: 'center',
+  },
+  body: {
+    width: '80%',
+  },
+  error: {
+    height: 30,
+    textAlign: 'center',
+    color: '#750000',
+    fontSize: 20,
+    fontWeight: 700,
+    textShadowColor: 'white',
+    textShadowRadius: 10,
+  },
+  inputContainer: {
+    backgroundColor: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  inputContainerDisabled: {
+    backgroundColor: '#c0c0c0',
+  },
+  input : {
+    width: '90%',
+    margin: 10,
+    fontSize: 16,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor : GLOBAL_COLOR.SECONDARY,
+  },
+  inputDisabled: {
+    backgroundColor: '#c0c0c0',
+  },
+  linkContainer: {
+    marginTop: -10,
+  },
+  linkText: {
+    color: 'white',
+    textDecorationLine: 'underline',
+  },
+  btnConnect: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: GLOBAL_COLOR.PRIMARY,
+  },
+  btnConnectDisabled: {
+    backgroundColor: '#c0c0c0',
+  },
+  textConnect: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  textConnectDisabled: {
+    color: '#c0c0c0',
+  },
 });
