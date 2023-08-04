4c3884edb04fb9afb42be935415171f5263144d3
Maxime Gui <maximeguillemin63@gmail.com>
2023-08-04 19:40:09 +0200

    modifs mineur niveau de TripScreen, HomeScreen, HomesStyle

--- screens/TripScreen.jsx
+++ screens/TripScreen.jsx
@@ -32,9 +32,6 @@ export default function TripScreen({ route, navigation }) {
   const events = useSelector((state) => state.events.value);
   const dispatch = useDispatch();
 
-  //gerer le style du buttonAdd
-  const buttonStyle = styles.boutonAdd;
-
   // 2. UseEffect, UseState, UseRef
   const [modalVisible, setModalVisible] = useState(false);
   // On récupère le trip dans le backend avec les events et infos, puis on sauvegarde dans le redux storage
@@ -162,7 +159,7 @@ export default function TripScreen({ route, navigation }) {
           </ScrollView>
           <BoutonAdd
             onPress={() => navigation.navigate("NewEvent",  { screen: "NewEvent", tokenTrip })}
-            buttonStyle={buttonStyle}
+            buttonStyle={styles.boutonAdd}
           />
         </View>
       </View>
