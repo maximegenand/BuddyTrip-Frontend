afbe56d517e22020cd81373173bfb557a0539f58
maxime.genand <m.genand@gmx.fr>
2023-08-08 23:23:52 +0200

    add modal when save and improve css

--- screens/NewTripScreen.jsx
+++ screens/NewTripScreen.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import {
   View,
   Text,
-  TextInput,
+  StatusBar,
   TouchableOpacity,
   KeyboardAvoidingView,
   Platform,
@@ -15,10 +15,12 @@ import { BACK_URL } from "@env";
 
 // Import styles
 import styles from "../styles/NewTripStyles";
+import { globalsStyles, GLOBAL_COLOR } from "../styles/globals";
 
 //Import components
 import HeaderNav from "../components/HeaderNewTrip";
 import AddBuddyTrip from "../components/AddBuddyTrip";
+import LoadingModal from "../components/LoadingModal";
 import InputComponent from "../components/Input";
 
 //Import modules
@@ -41,6 +43,7 @@ export default function NewTripScreen({ navigation }) {
 
   // Gère l'affichage de la modale
   const [modalBuddyVisible, setModalBuddyVisible] = useState(false);
+  const [modalLoadingVisible, setModalLoadingVisible] = useState(false);
   const [buddiesSelected, setBuddiesSelected] = useState([]);
 
   // États pour gérer les valeurs des champs
@@ -49,6 +52,11 @@ export default function NewTripScreen({ navigation }) {
   const [endDateText, setEndDateText] = useState("");
   const [description, setDescription] = useState("");
   const [textError, setTextError] = useState("");
+
+  // Permet de supprimer le message d'erreur dès que l'utilisateur tape un nouveau texte
+  useEffect(() => {
+    if (textError) setTextError(null);
+  }, [tripName, startDateText, endDateText, description])
 
   // On récupère les tokenUser de tous utilisateurs au chargement de la page
   const [ dataUsers, setDataUsers ] = useState([]);
@@ -84,7 +92,6 @@ export default function NewTripScreen({ navigation }) {
   // Fonction qui gère l'affichage ou non de la modale des buddies
   const handleModal = () => {
     setModalBuddyVisible(!modalBuddyVisible);
-    console.log(buddiesSelected)
   }
 
   // Fonction pour masquer le clavier lorsque l'utilisateur appuie en dehors du champ de saisie
@@ -103,18 +110,24 @@ export default function NewTripScreen({ navigation }) {
 
   // Fonction pour créer un nouveau groupe de voyage
   async function handleAddTrip() {
-    try {
+    // On annule l'action si la modale est affichée
+    if (modalLoadingVisible) return;
+    // On affiche la modale le temps du fetch
+    setModalLoadingVisible(true);
 
+    try {
       // Vérifier que tous les champs de l'objet sont non vides
       if (!tripName || !startDateText || !endDateText || !description) {
         console.log("Tous les champs doivent être remplis.");
-        setTextError("Tous les champs doivent être remplis");
+        setModalLoadingVisible(false);
+        setTextError("Formulaire incomplet");
         return;
       }
 
       // Vérifier la validité de la date de début
       if (!isValidDate(startDateText)) {
         console.log("La date de début est invalide.");
+        setModalLoadingVisible(false);
         setTextError("Date de début invalide");
         return;
       }
@@ -122,6 +135,7 @@ export default function NewTripScreen({ navigation }) {
       // Vérifier la validité de la date de fin
       if (!isValidDate(endDateText)) {
         console.log("La date de fin est invalide.");
+        setModalLoadingVisible(false);
         setTextError("Date de fin invalide");
         return;
       }
@@ -136,7 +150,8 @@ export default function NewTripScreen({ navigation }) {
       // Vérifier que la date de fin est supérieure à la date de début
       if (parsedEndDate <= parsedStartDate) {
         console.log("La date de fin doit être supérieure à la date de début.");
-        setTextError("La date de fin doit être supérieure à la date de début");
+        setModalLoadingVisible(false);
+        setTextError("Date de fin < Date de début");
         return;
       }
 
@@ -165,95 +180,69 @@ export default function NewTripScreen({ navigation }) {
       // Si la réponse du backend est true, rediriger l'utilisateur vers HomeScreen
       if (responseData.result === true) {
         dispatch(addTrip(responseData.trip));
-        navigation.navigate("Home");
+        await navigation.navigate("Home");
+        setModalLoadingVisible(false);
       }
     } catch (error) {
       console.error("Erreur lors de l'envoi du voyage au serveur :", error);
+      setModalLoadingVisible(false);
+      setTextError("Erreur de connexion au serveur");
     }
   }
 
   return (
-    <SafeAreaView style={styles.screen}>
+    <>
+      <StatusBar translucent={false} backgroundColor={GLOBAL_COLOR.PRIMARY} barStyle="light-content" />
+      <SafeAreaView style={{ flex: 0, backgroundColor: GLOBAL_COLOR.PRIMARY }} />
+      <LoadingModal visible={modalLoadingVisible} />
       <AddBuddyTrip modalVisible={modalBuddyVisible} data={dataUsers} setBuddiesSelected={setBuddiesSelected} buddiesSelected={buddiesSelected} handleModal={handleModal} />
-      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
-        <TouchableOpacity style={{ flex: 1 }} activeOpacity={1} onPress={dismissKeyboard}>
-          <HeaderNav navigation={navigation} />
-          <View style={styles.content}>
-            <InputComponent
-              key="name"
-              name="name"
-              placeholder="Nom du groupe"
-              onInputChange={handleInputChange}
-              value={tripName}
-            />
-            <InputComponent
-              key="dateStart"
-              name="dateStart"
-              type="date"
-              placeholder="Date de début (JJ/MM/AAAA)"
-              onInputChange={handleInputChange}
-              value={startDateText}
-            />
-            <InputComponent
-              key="dateEnd"
-              name="dateEnd"
-              type="date"
-              placeholder="Date de fin(JJ/MM/AAAA)"
-              onInputChange={handleInputChange}
-              value={endDateText}
-            />
-            <InputComponent
-              key="description"
-              name="description"
-              type="description"
-              placeholder="Description du voyage"
-              onInputChange={handleInputChange}
-              value={description}
-            />
-            
-{/*
-            <View style={styles.form}>
-              <Text style={styles.textmain}>Nom du Trip : {tripName}</Text>
-              <TextInput style={styles.input} placeholder="Nom du Groupe" onChangeText={setTripName} value={tripName} />
-            </View>
-            <View style={styles.form}>
-              <Text style={styles.textmain}>Date de début : {startDateText}</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Entrez une date (JJ/MM/AAAA)"
+      <SafeAreaView style={styles.screen}>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+          <TouchableOpacity style={{ flex: 1 }} activeOpacity={1} onPress={dismissKeyboard}>
+            <HeaderNav navigation={navigation} />
+            <View style={styles.content}>
+              <Text style={styles.textError}>{textError}</Text>
+              <InputComponent
+                key="name"
+                name="name"
+                placeholder="Nom du groupe"
+                onInputChange={handleInputChange}
+                value={tripName}
+              />
+              <InputComponent
+                key="dateStart"
+                name="dateStart"
+                type="date"
+                placeholder="Date de début (JJ/MM/AAAA)"
+                onInputChange={handleInputChange}
                 value={startDateText}
-                onChangeText={handleStartDateChange}
-                maxLength={10}
               />
-            </View>
-            <View style={styles.form}>
-              <Text style={styles.textmain}>Date de fin : {endDateText}</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Entrez une date (JJ/MM/AAAA)"
+              <InputComponent
+                key="dateEnd"
+                name="dateEnd"
+                type="date"
+                placeholder="Date de fin (JJ/MM/AAAA)"
+                onInputChange={handleInputChange}
                 value={endDateText}
-                onChangeText={handleEndDateChange}
-                maxLength={10}
               />
-            </View>
-            <View style={styles.form}>
-              <Text style={styles.textmain}>Description du voyage :</Text>
-              <TextInput
-                style={styles.descriptionInput}
-                placeholder="Saisissez la description de votre voyage ici..."
+              <InputComponent
+                key="description"
+                name="description"
+                type="description"
+                placeholder="Description du voyage"
+                onInputChange={handleInputChange}
                 value={description}
-                multiline={true}
-                onChangeText={setDescription}
               />
-  </View> */}
-            <TouchableOpacity onPress={handleModal}><Text style={{fontSize: 20, fontWeight: '700', paddingBottom: 20}}>Ajouter des buddies</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btnAdd} onPress={handleAddTrip}>
-              <Text style={styles.btnText}>Add Trip</Text>
-            </TouchableOpacity>
-            <Text style={styles.textError}>{textError}</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+              <TouchableOpacity style={styles.btnBuddy} onPress={handleModal}>
+                <Text style={styles.buddyText}>Sélectionner des buddies - <Text style={{fontWeight: "bold"}}>{buddiesSelected.length}/50</Text></Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.btnAdd} onPress={handleAddTrip}>
+                <Text style={styles.btnText}>Add Trip</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </>
   );
 }
