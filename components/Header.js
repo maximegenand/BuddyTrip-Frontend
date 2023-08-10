e5344b9522556bc81626ca7d6a8e2416c813d48a
maxime.genand <m.genand@gmx.fr>
2023-08-11 00:55:20 +0200

    change css

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,19 @@
-import { View, StyleSheet, Text, Dimensions, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { GLOBAL_COLOR } from '../styles/globals';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-const { width:screenWidth, height: screenHeight } = Dimensions.get('window');
 
 export default function Header({ navigation, title, participants, handlePress, style}) {
   return (
     <View style={[styles.header, style]}>
-      <View style={styles.header_Left}>
-        <TouchableOpacity onPress={() => navigation.navigate('Home')} activeOpacity={0.5}>
-          <FontAwesome style={styles.fleche} name="arrow-left" size={30} color={GLOBAL_COLOR.TERTIARY} />
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity style={styles.header_text} onPress={handlePress}>
-          <Text style={styles.name}>{title}</Text>
-          <Text style={styles.buddys}>{participants}</Text>
+      <TouchableOpacity style={styles.headerSide} onPress={() => navigation.navigate('Home')} activeOpacity={0.5}>
+        <FontAwesome name="arrow-left" size={30} color={GLOBAL_COLOR.TERTIARY} />
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.5}>
-        <FontAwesome style={styles.bell} name="bell" size={30} color={GLOBAL_COLOR.TERTIARY} />
+      <TouchableOpacity style={styles.headerCenter} onPress={handlePress}>
+          <Text numberOfLines={1} ellipsizeMode="middle" style={styles.title}>{title}</Text>
+          <Text style={styles.buddies}>{participants}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.headerSide} activeOpacity={0.5}>
+        <FontAwesome name="bell" size={30} color={GLOBAL_COLOR.TERTIARY} />
       </TouchableOpacity>
     </View>
   );
@@ -24,39 +21,30 @@ export default function Header({ navigation, title, participants, handlePress, s
 
 const styles = StyleSheet.create({
     header : {
-        backgroundColor : GLOBAL_COLOR.PRIMARY,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingLeft: 10,
-        paddingRight: 10,
-    },
-    header_Left : {
-        display: 'flex',
-        flexDirection: 'row',
-    },
-    header_text :{
-      flexDirection: 'column',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
       alignItems: 'center',
+      backgroundColor: GLOBAL_COLOR.PRIMARY,
     },
-    name: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      color: GLOBAL_COLOR.TERTIARY,
-      margin: 5,
+    headerSide: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 60,
+      height: 60,
     },
-    buddys : {
-        color : GLOBAL_COLOR.TERTIARY,
+    headerCenter: {
+      flex: 1,
+      gap: 5,
+      paddingHorizontal: 5,
+    },
+    title: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 700,
+    },
+    buddies: {
+        color : 'white',
         fontSize: 12,
         fontStyle: 'italic',
     },
-    fleche : {
-        marginRight: 10,
-    },
-    bell : {
-    },
-    header_text : {
-      maxWidth: 200
-    }
 });
